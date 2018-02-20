927c955771194da09766c1bf0a5158a8ef71fa57
FlorineC <miniflorine@hotmail.fr>
2018-02-20 08:51:37 +0100

    Import data for elasticsearch

--- elasticsearch/import.js
+++ elasticsearch/import.js
@@ -7,11 +7,43 @@ var esClient = new elasticsearch.Client({
   log: 'error'
 });
 
+// Création de l'indice
+esClient.indices.create({ index: '911calls' }, (err, resp) => {
+  if (err) console.trace(err.message);
+});
+
+let calls = [];
 fs.createReadStream('../911.csv')
-    .pipe(csv())
-    .on('data', data => {
-      // TODO extract one line from CSV
-    })
-    .on('end', () => {
-      // TODO insert data to ES
+  .pipe(csv())
+  .on('data', data => {
+    // TODO extract one line from CSV
+    calls.push({
+      lat: data.lat,
+      lng: data.lng,
+      desc: data.desc,
+      zip: data.zip,
+      title: data.title,
+      timeStamp: data.timeStamp,
+      twp: data.twp,
+      addr: data.addr
     });
+  })
+  .on('end', () => {
+    // TODO insert data to ES
+    esClient.bulk(createBulkInsertQuery(calls), (err, resp) => {
+      if (err) console.trace(err.message);
+      else console.log(`Inserted ${resp.items.length} calls`);
+      esClient.close();
+    });
+  });
+// Fonction utilitaire permettant de formatter les données pour l'insertion "bulk" dans elastic
+function createBulkInsertQuery(calls) {
+  const body = calls.reduce((acc, call) => {
+    const { lat, lng, desc, zip, title, timeStamp, twp, addr } = call;
+    acc.push({ index: { _index: '911calls', _type: 'call' } })
+    acc.push({ lat, lng, desc, zip, title, timeStamp, twp, addr })
+    return acc
+  }, []);
+
+  return { body };
+}
